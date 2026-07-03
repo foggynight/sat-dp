6a88959568f5af7908696a53ef27f327cbf49362
Robert Coffey <robertcoffey709@gmail.com>
2026-07-03 15:41:38 -0230

    add sat-dpll and move DP module into Main_DP

--- src/CNF.hs
+++ src/CNF.hs
@@ -1,8 +1,5 @@
 module CNF where
 
-import Data.List (nub)
-import Data.Maybe (mapMaybe)
-
 type Variable = Int    -- variable (> 0)
 type Literal = Variable  -- variable + sign
 type Clause = [Literal]
@@ -14,16 +11,6 @@ data CNF = CNF
 
 instance Show CNF where
   show cnf = show (cnf_clauses cnf)
-
-data Resolvent = Resolvent
-  { _res_p1 :: Clause   -- parent 1
-  , _res_p2 :: Clause   -- parent 2
-  , _res_res :: Clause  -- resolvent clause
-  }
-
-instance Show Resolvent where
-  show (Resolvent p1 p2 res) =
-    concat [(show p1), " ", (show p2), " -> ", (show res)]
 
 litHasVar :: Variable -> Literal -> Bool
 litHasVar var lit = (var == abs lit)
@@ -37,19 +24,3 @@ clauseHasVar var (lit:lits) = (abs lit == var) || clauseHasVar var lits
 clauseIsTautology :: Clause -> Bool
 clauseIsTautology [] = False
 clauseIsTautology (l:lits) = elem (-l) lits || clauseIsTautology lits
-
--- Generate var-resolvent given two clauses. Removes duplicate literals.
-resolve :: Variable -> Clause -> Clause -> Maybe Resolvent
-resolve var c1 c2 = do
-  if (elem var c1 && elem (-var) c2) || (elem (-var) c1 && elem var c2)
-  then let f = not . litHasVar var
-           res = nub $ filter f (c1 ++ c2)
-       in Just $ Resolvent c1 c2 res
-  else Nothing
-
--- Generate all var-resolvents given a list of clauses. Returns tuple of
-resolveAll :: Variable -> [Clause] -> [Resolvent]
-resolveAll _ [] = []
-resolveAll var (c:cs) =
-  let var_resolvents = mapMaybe (resolve var c) cs
-  in var_resolvents ++ resolveAll var cs
---
+++ src/Main_DPLL.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import CNF
+import DIMACS
+import Util
+
+-- DPLL ------------------------------------------------------------------------
+
+-- Main ------------------------------------------------------------------------
+
+main :: IO ()
+main = putStrLn "Hello, world."
